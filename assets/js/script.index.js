40b7f3e83ef892f291b8cdeaa8604c59f44dc86b
Lambda <122999532+lambdadotjoburg@users.noreply.github.com>
2023-01-18 17:55:15 +0200

    Add files via upload

---
+++ assets/js/script.index.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+function toggleHamburgerMenu(x) {
+    x.classList.toggle("change");
+}
